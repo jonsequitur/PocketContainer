117fdba4bb3d3e91aa3ef4f101d3b2d98e278631
jonsequitur <jonsequeira@gmail.com>
2017-10-04 21:55:45 -0700

    rewrite AfterCreating; clean up core code

--- Pocket.Container/PocketContainer.AfterCreating.cs
+++ Pocket.Container/PocketContainer.AfterCreating.cs
@@ -3,92 +3,151 @@
 
 using System;
 using System.Collections.Concurrent;
+using System.Linq;
 
 namespace Pocket
 {
     internal partial class PocketContainer
     {
-        private Func<PocketContainer, object> Pipe<T>(
-            Delegate resolve,
-            Func<T, T> transform)
-        {
-            var applied = false;
-
-            var f = (Func<PocketContainer, object>) resolve;
-
-            return container =>
-            {
-                var resolved = f(container);
-
-                if (singletons.TryGetValue(typeof(T), out var existing) &&
-                    existing.Equals(resolved))
-                {
-                    if (!applied)
-                    {
-                        applied = true;
-                        var transformed = transform((T) resolved);
-                        singletons.TryUpdate(typeof(T), transformed, resolved);
-                        resolved = transformed;
-                    }
-                }
-                else
-                {
-                    var replaced = transform((T) resolved);
-
-                    if (replaced != null)
-                    {
-                        resolved = replaced;
-                    }
-                }
-
-                return resolved;
-            };
-        }
-
-        private readonly ConcurrentDictionary<Type, Delegate> afterCreatingStash = new ConcurrentDictionary<Type, Delegate>();
-
         public PocketContainer AfterCreating<T>(Func<T, T> transform)
         {
-            if (resolvers.TryGetValue(typeof(T), out var existing))
-            {
-                var f = Pipe(existing, transform);
-                if (f != null)
-                {
-                    resolvers.TryUpdate(typeof(T), f, existing);
-                }
-            }
-            else
-            {
-                afterCreatingStash.AddOrUpdate(
-                    typeof(T),
-                    t => transform,
-                    (t, first) => new Func<T, T>(x =>
-                    {
-                        var transformed = ((Func<T, T>) first)(x);
-                        return transform(transformed);
-                    }));
-            }
+            TryRegisterSingle(c =>
+                                  new AfterResolvePipeline<T>(this, ExistingResolver<T>()));
 
-            Registering += f =>
-            {
-                if (afterCreatingStash.TryGetValue(typeof(T), out var stashed))
-                {
-                    return Pipe<T>(f, c => ((Func<T, T>) stashed).Invoke(c));
-                }
+            var pipeline = Resolve<AfterResolvePipeline<T>>();
 
-                return null;
-            };
+            pipeline.Transforms.Enqueue(transform);
+
+            resolvers[typeof(T)] = c => pipeline.Resolve();
+
+            if (pipeline.Transforms.Count == 1)
+            {
+                Registering += d => Reregister((dynamic) d);
+            }
 
             return this;
         }
 
-        public PocketContainer AfterCreating<T>(Action<T> then)
+        private Func<PocketContainer, object> Reregister<T>(Func<PocketContainer, T> newResolverDelegate)
         {
-            return AfterCreating<T>(resolved =>
+            if (singletons.TryGetValue(typeof(AfterResolvePipeline<T>), out var pipelineObj))
+            {
+                var pipeline = (AfterResolvePipeline<T>) pipelineObj;
+
+                if (newResolverDelegate is Func<PocketContainer, T> newResolver)
+                {
+                    var resolverType = newResolver.GetType().GenericTypeArguments[1];
+                    var pipelineType = pipeline.GetType().GetGenericArguments()[0];
+                    if (resolverType == pipelineType)
+                    {
+                        //                    Console.WriteLine($"RE-registering {transform} for {pipeline.ResolvesType}");
+
+                        pipeline.InnerResolver = newResolver;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"[HMMM] wanting to replace {resolverType} but pipeline is {pipelineType}");
+                    }
+
+                    return c => pipeline.Resolve();
+                }
+            }
+
+            return null;
+        }
+
+        public PocketContainer AfterCreating<T>(Action<T> then) =>
+            AfterCreating<T>(resolved =>
             {
                 then(resolved);
                 return resolved;
             });
+
+        private class AfterResolvePipeline<T>
+        {
+            public readonly ConcurrentQueue<Func<T, T>> Transforms = new ConcurrentQueue<Func<T, T>>();
+
+            private readonly PocketContainer container;
+
+            private Func<PocketContainer, T> innerResolver;
+            private bool transformsApplied;
+
+            public AfterResolvePipeline(
+                PocketContainer container,
+                Func<PocketContainer, object> originalResolver)
+            {
+                this.container = container ?? throw new ArgumentNullException(nameof(container));
+
+                if (originalResolver != null)
+                {
+                    InnerResolver = c => (T) originalResolver(c);
+                }
+            }
+
+            public Type ResolvesType { get; } = typeof(T);
+
+            public Func<PocketContainer, T> InnerResolver
+            {
+                private get
+                {
+                    if (innerResolver == null)
+                    {
+                        innerResolver = c =>
+                        {
+                            var implicitResolver = container.ImplicitResolver<T>();
+                            return (T) implicitResolver(container);
+                        };
+                    }
+
+                    return innerResolver;
+                }
+                set
+                {
+                    innerResolver = value;
+                }
+            }
+
+            public T Resolve()
+            {
+                var instance = InnerResolver(container);
+
+                var isSingleton =
+                    container.singletons.TryGetValue(typeof(T), out var existing) &&
+                    existing.Equals(instance);
+
+                foreach (var transform in Transforms)
+                {
+                    if (isSingleton)
+                    {
+                        if (!transformsApplied)
+                        {
+                            var transformed = transform(instance);
+                            if (transformed != null)
+                            {
+                                container.singletons.TryUpdate(typeof(T), transformed, instance);
+                                instance = transformed;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var transformed = transform(instance);
+                        if (transformed != null)
+                        {
+                            instance = transformed;
+                        }
+                    }
+                }
+
+                transformsApplied = true;
+
+                return instance;
+            }
         }
+
+        private Func<PocketContainer, object> ExistingResolver<T>() =>
+            this.Where(f => f.Key == typeof(T))
+                .Select(pair => pair.Value)
+                .SingleOrDefault();
     }
 }
